0fe12ba24869e36503df30cd1220479028c9e536
IkramZargar <ikramxargar@gmail.com>
2024-03-27 13:55:11 +0530

    add firebase auth and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:skill_hire/screens/intro.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
 
--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -14,79 +14,77 @@ class _IntroScreenState extends State<IntroScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: RichText(
-                text: TextSpan(
-                  text: 'Skill',
-                  style: AppTextStyles.heading1(),
-                  children: [
-                    TextSpan(
-                      text: 'Hire',
-                      style: AppTextStyles.heading1()
-                          .copyWith(color: Colors.green),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Center(child: Image.asset('images/logo.png')),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Text(
-                'It`s',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: RichText(
+              text: TextSpan(
+                text: 'Skill',
                 style: AppTextStyles.heading1(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: AnimatedTextKit(
-                animatedTexts: [
-                  TyperAnimatedText(
-                    'Fast',
-                    textStyle: AppTextStyles.heading1().copyWith(
-                      color: Colors.green,
-                    ),
-                  ),
-                  TyperAnimatedText(
-                    'Reliable',
-                    textStyle: AppTextStyles.heading1().copyWith(
-                      color: Colors.green,
-                    ),
-                  ),
-                  TyperAnimatedText(
-                    'Secure',
-                    textStyle: AppTextStyles.heading1().copyWith(
-                      color: Colors.green,
-                    ),
+                children: [
+                  TextSpan(
+                    text: 'Hire',
+                    style: AppTextStyles.heading1()
+                        .copyWith(color: Colors.green),
                   ),
                 ],
-                pause: const Duration(milliseconds: 1000),
-                displayFullTextOnTap: true,
-                stopPauseOnTap: true,
-                repeatForever: true,
               ),
             ),
-            const SizedBox(height: 50,),
-            Center(
-              child: MaterialButton(
-                color: Colors.green,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+          ),
+          Center(child: Image.asset('images/logo.png')),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Text(
+              'It`s',
+              style: AppTextStyles.heading1(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TyperAnimatedText(
+                  'Fast',
+                  textStyle: AppTextStyles.heading1().copyWith(
+                    color: Colors.green,
+                  ),
                 ),
-                onPressed: () {},
-                child: const  Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Let`s Start',style: TextStyle(fontSize: 30),),
+                TyperAnimatedText(
+                  'Reliable',
+                  textStyle: AppTextStyles.heading1().copyWith(
+                    color: Colors.green,
+                  ),
                 ),
+                TyperAnimatedText(
+                  'Secure',
+                  textStyle: AppTextStyles.heading1().copyWith(
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+              pause: const Duration(milliseconds: 1000),
+              displayFullTextOnTap: true,
+              stopPauseOnTap: true,
+              repeatForever: true,
+            ),
+          ),
+          const SizedBox(height: 50,),
+          Center(
+            child: MaterialButton(
+              color: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              onPressed: () {},
+              child: const  Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Let`s Start',style: TextStyle(fontSize: 30),),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
